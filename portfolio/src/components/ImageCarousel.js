a7c0245b09d4714ad809f0989584fa5b2bf769ae
usama-asghar1 <usama.asghar@hotmail.co.uk>
2023-08-02 19:10:23 +0100

    image carousel zoom added

--- portfolio/src/components/ImageCarousel.js
+++ portfolio/src/components/ImageCarousel.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -25,34 +27,62 @@ function NextArrow(props) {
     );
   }
   
-const ImageCarousel = ({ images }) => {
+  const ImageCarousel = ({ images }) => {
+    const [enlargedImage, setEnlargedImage] = useState(null);
+  
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      nextArrow: <NextArrow />,
+      prevArrow: <PrevArrow />,
+    };
+  
+    useEffect(() => {
+      const bodyOverflow = enlargedImage ? 'hidden' : 'auto';
+      document.body.style.overflow = bodyOverflow;
+  
+      return () => {
+        document.body.style.overflow = 'auto';
+      };
+    }, [enlargedImage]);
 
+    const enlargeImage = (url) => {
+      setEnlargedImage(url);
+    };
+  
+    const handleClose = () => {
+      setEnlargedImage(null);
+    };
+  
+    return (
+      <div className="flex items-center justify-center mt-10 mb-16">
+        <div className="w-1/2">
+          <Slider {...settings}>
+            {images.map((imageInfo, index) => (
+              <div key={index} className="text-center" onClick={() => enlargeImage(imageInfo.url)}>
+                <h1 className="text-center font-bold text-2xl mb-4">{imageInfo.alt}</h1>
+                <img src={imageInfo.url} alt={imageInfo.alt} className='shadow-xl' />
+              </div>
+            ))}
+          </Slider>
+        </div>
+        {enlargedImage && (
+        <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center">
+          <div className="max-w-3/4 relative">
+            <img src={enlargedImage} alt="Enlarged" className="w-full h-auto max-h-96" />
+            <button className="absolute top-2 right-2 text-red-500 bg-white border border-red-500 px-3 py-1 rounded-lg" onClick={handleClose}>
+              CLOSE
+            </button>
+          </div>
+        </div>
+      )}
 
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    nextArrow: <NextArrow />,
-    prevArrow: <PrevArrow />,
-  };
-
-
-  return (
-    <div className="flex items-center justify-center mt-10 mb-16">
-      <div className="w-1/2">
-        <Slider {...settings}>
-          {images.map((imageInfo, index) => (
-            <div key={index} className="text-center">
-              <h1 className="text-center font-bold text-2xl mb-4">{imageInfo.alt}</h1>
-              <img src={imageInfo.url} alt={imageInfo.alt} className='shadow-xl'/>
-            </div>
-          ))}
-        </Slider>
       </div>
-    </div>
-  );
-};
-
+    );
+  };
+  
+  
 export default ImageCarousel;
